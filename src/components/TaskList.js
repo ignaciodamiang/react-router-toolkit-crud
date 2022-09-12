8f031715dbce79ff88b7c29df7d34a7bcb0bb5af
Ignacio Damián González <ignaciodamiang@gmail.com>
2022-09-11 21:57:49 -0300

    first commit

---
+++ src/components/TaskList.js
@@ -0,0 +1,50 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { deleteTask } from '../features/tasks/taskSlice';
+import { Link } from 'react-router-dom';
+
+function TaskList() {
+  const tasks = useSelector((state) => state.tasks);
+  const dispatch = useDispatch();
+  const handleDelete = (id) => {
+    dispatch(deleteTask(id));
+  };
+  return (
+    <div className="w-4/6">
+      <header className="flex justify-between items-center py-4">
+        <h1>{tasks.length} tasks</h1>
+        <Link
+          to="/create-task"
+          className="bg-indigo-800 px-2 py-1 text-sm rounded"
+        >
+          Create Task
+        </Link>
+      </header>
+      <div className="grid grid-cols-3 gap-4">
+        {tasks.map((task) => (
+          <div key={task.id} className="bg-neutral-800 p-4 rounded-md">
+            <header className="flex justify-between">
+              <h3>{task.title}</h3>
+              <div className="flex gap-x-2">
+                <Link
+                  to={`/edit-task/${task.id}`}
+                  className="bg-zinc-600 px-2 py-1 text-xs rounded-md"
+                >
+                  Edit
+                </Link>
+                <button
+                  onClick={() => handleDelete(task.id)}
+                  className="bg-red-500 px-2 py-1 text-xs rounded-md "
+                >
+                  delete
+                </button>
+              </div>
+            </header>
+            <p>{task.description}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default TaskList;
